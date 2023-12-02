b12c92b07dff8e7825eedd1d00d21f1b7cf18622
Даня <daniilevgen79@gmail.com>
2023-12-03 00:06:26 +0300

    в header__title была одна земля. Исправил --font-size-bigest -> --font-size- big добавил стили первой странице

--- custom-elements/header.js
+++ custom-elements/header.js
@@ -4,7 +4,7 @@ class HeaderTemplate extends HTMLElement {
   }
 
   connectedCallback() {
-    this.innerHTML = '<header class="header"><h1 class="header_title">Канбан-доска</h1><div class="header__user"><img src="img/Avatar.png" alt="Аватар" class="user__avatar" /><span class="user__name">user name</span><span class="user__logOutBtn">▼</span><div class="user__logOut user__logOut-close"><img src="img/Exit.svg" alt="Иконка выхода" /><span class="logOut__title">Выйти</span></div></div> </header>'
+    this.innerHTML = '<header class="header"><h1 class="header__title">Канбан-доска</h1><div class="header__user"><img src="img/Avatar.png" alt="Аватар" class="user__avatar" /><span class="user__name">user name</span><span class="user__logOutBtn">▼</span><div class="user__logOut user__logOut-close"><img src="img/Exit.svg" alt="Иконка выхода" /><span class="logOut__title">Выйти</span></div></div> </header>'
   }
 }
 
