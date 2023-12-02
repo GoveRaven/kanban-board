90973d0d1b59e794f1341a248d9b44ad8f826cf9
Даня <daniilevgen79@gmail.com>
2023-12-02 17:43:49 +0300

    Добавил первую страницу и перетащил все файлы из папки template

---
+++ templates.js
@@ -0,0 +1,24 @@
+// Шаблон шапки
+class templateOFHeader extends HTMLElement {
+  constructor() {
+    super()
+  }
+
+  connectedCallback() {
+    this.innerHTML = '<header class="header"><h1 class="header_title">Канбан-доска</h1><div class="header__user"><img src="img/Avatar.png" alt="Аватар" class="user__avatar" /><span class="user__name">user name</span><span class="user__logOutBtn">▼</span><div class="user__logOut user__logOut-close"><img src="img/Exit.svg" alt="Иконка выхода" /><span class="logOut__title">Выйти</span></div></div> </header>'
+  }
+}
+
+customElements.define("template-header", templateOFHeader);
+
+class templateOFTasks extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = '<div class="task"><span></span></div>';
+  }
+}
+
+customElements.define("template-task", templateOFTasks);
