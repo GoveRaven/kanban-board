80615e2477a3eeae9b1a0329fdb642db0531f765
Даня <daniilevgen79@gmail.com>
2023-12-14 22:10:14 +0300

    добавил второй странице проверку авторизации

--- js/logOut.js
+++ js/logOut.js
@@ -1,12 +1,21 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
 import {
   getAuth,
+  onAuthStateChanged,
   signOut,
 } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 import { firebaseConfig } from "/js/firebaseConfig.js";
 
 initializeApp(firebaseConfig);
 const auth = getAuth();
+const redirectedPage = "/index.html"
+
+onAuthStateChanged(auth, () => {
+  if (!auth.currentUser) {
+    window.location.href = redirectedPage;
+  }
+});
+
 const logOutBtn = document.querySelector(".user__logOut");
 
 logOutBtn.addEventListener("click", singOutGitHub);
@@ -14,5 +23,6 @@ logOutBtn.addEventListener("click", singOutGitHub);
 function singOutGitHub() {
   signOut(auth);
   logOutBtn.removeEventListener("click", singOutGitHub);
-  window.location.href = "/index.html";
+  window.location.href = redirectedPage;
 }
+
