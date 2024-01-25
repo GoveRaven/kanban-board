bcdaba0400de09352e16fba59f83b7054e3454d7
Даня <daniilevgen79@gmail.com>
2024-01-26 00:58:08 +0300

    убрал добавление одного класса добавил импорт и событие двух функция для drag'n'drop

--- scripts/tasks-scripts/createTask.js
+++ scripts/tasks-scripts/createTask.js
@@ -1,16 +1,19 @@
+import {drag, drop} from "./dragAndDrop.js"
+
 const taskName = document.querySelector(".new-task__name");
 const addTaskForm = document.querySelector(".new-task__form");
 const list = document.querySelector(".tasks__list_backlog");
-
-console.log(addTaskForm);
 
 function createTask(event) {
   event.preventDefault();
   const task = document.createElement("template-task");
   list.append(task);
   const text = task.querySelector(".task__text");
-  task.classList.add("task", "task_backlog");
+  task.classList.add("task");
   text.textContent = taskName.value;
+  task.draggable="true"
+  task.addEventListener("dragstart", () => drag(task));
+  task.addEventListener("dragend", () => drop(task));
   taskName.value = "";
 }
 
