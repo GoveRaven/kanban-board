4a3889fe58daa51d0e31504c62439f11d524a454
Даня <daniilevgen79@gmail.com>
2024-01-26 22:17:04 +0300

    импортирует сами задачи, а не списки переписал условие для функции endEditTaskWithKeyDown переместил removeEventListener из endEditTask в функции, которые они же и убирают переписал editTask. Теперь она не делегированная, а весит на каждой задаче + можно редактировать только 1 задачу за раз добавил экспорт функции

--- scripts/tasks-scripts/editTask.js
+++ scripts/tasks-scripts/editTask.js
@@ -1,28 +1,34 @@
-import { tasksLists } from "./routes.js";
+import { tasks } from "./routes.js";
 
 function endEditTask(task, text) {
   task.classList.remove("task_editing");
   text.removeAttribute("contenteditable");
-  window.removeEventListener("keydown", endEditTaskWithKeyDown);
-  window.removeEventListener("click", endEditTaskWithClick);
 }
 
 function endEditTaskWithKeyDown(event, task, text) {
-  if (event.key === "Enter" || event.key === "Escape") {
+  if (["Enter", "Escape"].includes(event.key)) {
     event.preventDefault();
     endEditTask(task, text);
+    window.removeEventListener("keydown", (event) =>
+      endEditTaskWithKeyDown(event, task, text)
+    );
   }
 }
 
 function endEditTaskWithClick(event, task, text) {
   if (!event.target.closest(".task")) {
     endEditTask(task, text);
+    window.removeEventListener("click", (event) =>
+      endEditTaskWithClick(event, task, text)
+    );
   }
 }
 
-function editTask(event) {
-  if (!event.target.classList.contains("task__icon")) return;
-  const task = event.target.closest(".task");
+function editTask(event, task) {
+  const smthIsEditing = document.querySelector(".task_editing");
+  if (smthIsEditing) {
+    smthIsEditing.classList.remove("task_editing");
+  }
   const text = task.querySelector(".task__text");
   task.classList.add("task_editing");
   text.setAttribute("contenteditable", "true");
@@ -35,4 +41,8 @@ function editTask(event) {
   );
 }
 
-tasksLists.forEach((el) => el.addEventListener("click", editTask));
+tasks.forEach((task) =>
+  task.addEventListener("click", (event) => editTask(event, task))
+);
+
+export { editTask };
