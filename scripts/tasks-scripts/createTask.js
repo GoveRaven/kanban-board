fae9ef19674dc591964ca4de419a5c2bd99f748c
Даня <daniilevgen79@gmail.com>
2024-01-26 22:18:19 +0300

    добавил экспорт задач по перетаскиванию и редактированию задач переименовал переменную добавил подписание задачек на события перемещения и редактирования

--- scripts/tasks-scripts/createTask.js
+++ scripts/tasks-scripts/createTask.js
@@ -1,20 +1,22 @@
-import {drag, drop} from "./dragAndDrop.js"
+import { drag, drop} from "./dragAndDrop.js";
+import {editTask} from './editTask.js'
 
-const taskName = document.querySelector(".new-task__name");
-const addTaskForm = document.querySelector(".new-task__form");
+const taskForm = document.querySelector(".new-task__form");
+const taskName = taskForm.querySelector(".new-task__name");
 const list = document.querySelector(".tasks__list_backlog");
 
 function createTask(event) {
   event.preventDefault();
   const task = document.createElement("template-task");
   list.append(task);
-  const text = task.querySelector(".task__text");
   task.classList.add("task");
+  const text = task.querySelector(".task__text");
   text.textContent = taskName.value;
-  task.draggable="true"
+  task.draggable = "true";
   task.addEventListener("dragstart", () => drag(task));
   task.addEventListener("dragend", () => drop(task));
+  task.addEventListener("click", (event) => editTask(event, task));
   taskName.value = "";
 }
 
-addTaskForm.addEventListener("submit", (event) => createTask(event));
+taskForm.addEventListener("submit", createTask);
