768362de407a84bfb5b88259bddfc0840a84182f
Даня <daniilevgen79@gmail.com>
2024-01-26 22:53:12 +0300

    переместил цикл под функции переименовал getNextElem в getNextElement

--- scripts/tasks-scripts/dragAndDrop.js
+++ scripts/tasks-scripts/dragAndDrop.js
@@ -1,12 +1,7 @@
 import { tasksLists, tasks } from "./routes.js";
 
 tasksLists.forEach((list) => {
-  list.addEventListener("dragover", (event) => dragover(event));
-});
-
-tasks.forEach((task) => {
-  task.addEventListener("dragstart", () => drag(task));
-  task.addEventListener("dragend", () => drop(task));
+  list.addEventListener("dragover", dragover);
 });
 
 function drag(task) {
@@ -17,19 +12,24 @@ function drop(task) {
   task.classList.remove("task_dragndrop");
 }
 
+tasks.forEach((task) => {
+  task.addEventListener("dragstart", () => drag(task));
+  task.addEventListener("dragend", () => drop(task));
+});
+
 function dragover(event) {
   const list = event.target.closest(".tasks__list");
-  const selectedTask = document.querySelector(".task_dragndrop");
   const currentTask = event.target;
+  const selectedTask = document.querySelector(".task_dragndrop");
   const text = selectedTask.innerHTML;
-  const nextElement = getNextElem(event.clientY, currentTask)
+  const nextElement = getNextElement(event.clientY, currentTask)
   if (currentTask.classList.contains("task")) {
     list.insertBefore(selectedTask, nextElement);
     selectedTask.innerHTML = text;
   }
 }
 
-function getNextElem(cursorPosition, currentElement) {
+function getNextElement(cursorPosition, currentElement) {
   const currentElementCoord = currentElement.getBoundingClientRect();
   const currentElementCenter =
     currentElementCoord.y + currentElementCoord.height / 2;
