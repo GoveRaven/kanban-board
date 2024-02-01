124550e3ddf390027ba08c8fab7792c75e5e6302
Даня <daniilevgen79@gmail.com>
2024-02-01 17:56:25 +0300

    импортировал и добавил фукнцию добавления/стирание 'пустых' задач

--- scripts/tasks-scripts/createTask.js
+++ scripts/tasks-scripts/createTask.js
@@ -1,5 +1,6 @@
 import { drag, drop } from "./dragAndDrop.js";
 import { editTask } from "./editTask.js";
+import { checkNoTask } from "./createEmptyTask.js";
 
 const taskForm = document.querySelector(".new-task__form");
 const taskName = taskForm.querySelector(".new-task__name");
@@ -16,6 +17,7 @@ function createTask(event) {
   task.addEventListener("dragend", () => drop(task));
   task.addEventListener("click", (event) => editTask(event, task));
   taskName.value = "";
+  checkNoTask(list);
 }
 
 taskForm.addEventListener("submit", createTask);
