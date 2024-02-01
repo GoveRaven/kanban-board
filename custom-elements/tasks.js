b37e24e8579ddf8792fbbde6c4914d408a1616a5
Даня <daniilevgen79@gmail.com>
2024-02-01 17:55:27 +0300

    добавил условия для 'пустых' ззадач

--- custom-elements/tasks.js
+++ custom-elements/tasks.js
@@ -4,21 +4,26 @@ class TasksTemplate extends HTMLElement {
   }
 
   connectedCallback() {
-    this.draggable = "true"
+    this.dataset.empty !== "true" ?
+    this.draggable = "true" : ``
     this.innerHTML = `
     <span class="task__text" tabindex="0"></span>
-    <svg
-      width="18"
-      height="17"
-      viewBox="0 0 18 17"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      class="task__icon"
-    >
-      <path
-        d="M3.156 10.563L1 16.0176L6.74957 14.8753M3.156 10.563L6.74957 14.8753M3.156 10.563L11.7806 1.93845M6.74957 14.8753L16.0929 6.25074M11.7806 1.93845L16.0929 6.25074M11.7806 1.93845C12.1448 1.57425 14.6556 0.205922 16.0929 1.64329C17.5301 3.08065 17.0511 5.29246 16.0929 6.25074"
-      />
-    </svg>
+    ${
+      this.dataset.empty !== "true"
+        ? `    <svg
+    width="18"
+    height="17"
+    viewBox="0 0 18 17"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="task__icon"
+  >
+    <path
+      d="M3.156 10.563L1 16.0176L6.74957 14.8753M3.156 10.563L6.74957 14.8753M3.156 10.563L11.7806 1.93845M6.74957 14.8753L16.0929 6.25074M11.7806 1.93845L16.0929 6.25074M11.7806 1.93845C12.1448 1.57425 14.6556 0.205922 16.0929 1.64329C17.5301 3.08065 17.0511 5.29246 16.0929 6.25074"
+    />
+  </svg>`
+        : ``
+    }
   `;
   }
 }
