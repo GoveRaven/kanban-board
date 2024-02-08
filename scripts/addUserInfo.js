f8e68c53c0f25b5c4e30e8810c394a9d507206c9
Даня <daniilevgen79@gmail.com>
2024-02-08 15:47:10 +0300

    улучшил добавление информации о пользователе на страницу

--- scripts/addUserInfo.js
+++ scripts/addUserInfo.js
@@ -9,8 +9,6 @@ function addInfo(currentUser) {
   userAvatar.src = currentUser.photoURL;
 }
 
-setTimeout(() => addInfo(auth.currentUser.providerData[0]), 1000);
-
 const logOutBtn = document.querySelector(".user__logOutBtn");
 const logOut = document.querySelector(".user__logOut");
 
@@ -18,3 +16,5 @@ user.addEventListener("click", () => {
   logOut.classList.toggle("user__logOut-close");
   logOutBtn.classList.toggle("user__logOutBtn-open");
 });
+
+export { addInfo };
--- scripts/routes.js
+++ scripts/routes.js
@@ -1,17 +1,23 @@
 import { auth } from "./firebaseConfig.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
 import { removeLoader } from "./removeLoader.js";
+// import { addInfo } from "./addUserInfo.js";
 
 const hrefOfMainPage = "./index.html";
 const hrefOfLoginPage = "./login.html";
 
 onAuthStateChanged(auth, () => {
-  if (!auth.currentUser && window.location.href.includes("/login.html")) {
+  const user = auth.currentUser
+  if (!user && window.location.href.includes("/login.html")) {
     redirectToMainPage();
-  } else if (auth.currentUser && window.location.href.includes("/index.html")) {
+  } else if (user && window.location.href.includes("/index.html")) {
     redirectToLoginPage();
+  } else if (user) {
+    import("./addUserInfo.js").then((script) =>
+      script.addInfo(user.providerData[0])
+    );
   }
-  setTimeout(removeLoader, 1000);
+  removeLoader();
 });
 
 function redirectToMainPage() {
