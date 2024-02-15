46cc4ebbc9e84f749b87e04bf9bcfb167b2dbf88
Даня <daniilevgen79@gmail.com>
2024-02-15 20:45:08 +0300

    убрал функцию, отвеяающая за проверку состояния авторизации

--- scripts/routes.js
+++ scripts/routes.js
@@ -1,26 +1,5 @@
-import { auth } from "./firebaseConfig.js";
-import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js";
-import { removeLoader } from "./removeLoader.js";
-import { getUserFromDB } from "./cloudStorage.js";
-// import { addInfo } from "./addUserInfo.js";
-
 const hrefOfMainPage = "./index.html";
 const hrefOfLoginPage = "./login.html";
-
-onAuthStateChanged(auth, () => {
-  const user = auth.currentUser
-  if (!user && window.location.href.includes("/login.html")) {
-    redirectToMainPage();
-  } else if (user && window.location.href.includes("/index.html")) {
-    redirectToLoginPage();
-  } else if (user) {
-    import("./addUserInfo.js").then((script) =>
-      script.addInfo(user.providerData[0])
-    );
-    getUserFromDB(user.providerData[0].uid)
-  }
-  removeLoader();
-});
 
 function redirectToMainPage() {
   window.location.href = hrefOfMainPage;
@@ -35,5 +14,4 @@ export {
   hrefOfLoginPage,
   redirectToMainPage,
   redirectToLoginPage,
-  onAuthStateChanged,
 };
