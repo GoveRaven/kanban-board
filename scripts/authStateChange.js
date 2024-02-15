86567dc0cb8b0cc4a4792753bf888e149ddbc1f2
Даня <daniilevgen79@gmail.com>
2024-02-15 20:45:56 +0300

    переместил фукнцию проверки статуса авторизации в отдельный файл

---
+++ scripts/authStateChange.js
@@ -0,0 +1,21 @@
+import { auth } from "./firebaseConfig.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+import { removeLoader } from "./removeLoader.js";
+import { redirectToMainPage, redirectToLoginPage } from "./routes.js";
+
+onAuthStateChanged(auth, () => {
+  const user = auth.currentUser;
+  if (!user && window.location.href.includes("/login.html")) {
+    redirectToMainPage();
+  } else if (user && window.location.href.includes("/index.html")) {
+    redirectToLoginPage();
+  } else if (user) {
+    import("./addUserInfo.js").then((script) =>
+      script.addInfo(user.providerData[0])
+    );
+    import("./cloudStorage.js").then((script) =>
+      script.getUserFromDB(user.providerData[0].uid)
+    );
+  }
+  removeLoader();
+});
